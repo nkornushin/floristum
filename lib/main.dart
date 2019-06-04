9dbf15f03074c2302d99af7a1683b4f136b9cc69
unknown <nkornushin@gmail.com>
2019-06-04 13:00:59 +0300

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+
+    return new MaterialApp(
+      routes: {
+         "/": (_) => new SafeArea(
+              child: new WebviewScaffold(
+                url: "https://floristum.ru/",
+                hidden: false,
+              ),
+            ),
+        
+            
+        
+      },
+    );
+  }
+}
